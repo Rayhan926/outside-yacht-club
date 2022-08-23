d4272d3ed8038972b344f3367040e62ff213c2eb
rayhan926 <rayhanbd7075@gmail.com>
2022-08-23 20:37:55 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        "open-sans": ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#FFF24D",
+        secondary: "#8743CF",
+        dark: "#040503",
+        "dark-soft": "#1D1D1D",
+        gray: "#4A4A4A",
+      },
+    },
+  },
+  plugins: [],
+};
